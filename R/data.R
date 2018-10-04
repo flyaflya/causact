4da8bf7de5820ec567f91945e876917578c5d2f2
Fleischhacker <ajf@udel.edu>
2018-10-04 14:50:13 -0400

    add gym data and corruption data

--- R/data.R
+++ R/data.R
@@ -53,3 +53,34 @@
 #' }
 "ticketsDF"
 
+#' Dataframe of 44 observations of free crossfit classes data
+#' Each observation indicates how many students that participated in the free
+#' month of crossfit signed up for the monthly membership afterwards
+#' @format A data frame with 44 rows and 5 variables:
+#' \describe{
+#'   \item{gymID}{unique gym identifier}
+#'   \item{nTrialCustomers}{number of unique customers taking free trial classes}
+#'   \item{nSigned}{number of customers from trial that sign up for membership}
+#'   \item{yogaStretch}{whether trial classes included a yoga type stretch}
+#'   \item{timePeriod}{month number, since inception of company, for which trial period was offered}
+#' }
+"gymDF"
+
+#' Dataframe of 174 observations where information on the human developmet index (HDI)
+#' and the corruption perceptions index (CPI) both exist.
+#' Each observation is a country.
+#' @format A data frame with 174 rows and 7 variables:
+#' \describe{
+#'   \item{country}{country name}
+#'   \item{region}{region name as given with CPI rating}
+#'   \item{countryCode}{three letter abbreviation for country}
+#'   \item{regionCode}{four letter or less abbreviation for country}
+#'   \item{population}{2017 country population}
+#'   \item{CPI2017}{The Corruption Perceptions Index score for 2017: A country/territory’s score indicates the perceived level of public sector corruption on a scale of 0-100, where 0 means that a country is perceived as highly corrupt and a 100 means that a country is perceived as very clean. }
+#'   \item{HDI2017}{The human development index score for 2017: the Human Development Index (HDI) is a measure of achievement in the basic dimensions of human development across countries.  It is an index made from a simple unweighted average of a nation’s longevity, education and income and is widely accepted in development discourse.}
+#' }
+#' @source \url{http://www.transparency.org/cpi} CPI data available from www.transparency.org/cpi.  Accessed Oct 1, 2018. Consumer Perception Index 2017 by Transparency International is licensed under CC-BY- ND 4.0.
+#' @source \url{http://hdr.undp.org/en/content/human-development-index-hdi} HDA data accessed on Oct 1, 2018.
+#' @source \url{https://data.worldbank.org/indicator/sp.pop.totl} Population data accessed on Oct 1, 2018.
+"corruptDF"
+
