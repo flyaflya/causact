62cd6b4c75426e1cba315abacb513a444b03880a
Fleischhacker <ajf@udel.edu>
2019-03-07 14:58:46 -0500

    update

---
+++ R/autoLabel.R
@@ -0,0 +1,22 @@
+### function that populates auto labels and decriptions for a
+### graphical picture of the DAG
+autoLabel = function(graph){
+
+  ### start with data ... for now, exclude DF name from label
+  ### uses dfColToJustCol function from utils.R
+  graph$nodes_df$auto_data = dfColToJustCol(graph$nodes_df$data)
+
+  ### if description is NA, use data value
+  graph$nodes_df$auto_descr = ifelse(is.na(graph$nodes_df$descr),
+                                graph$nodes_df$auto_data,
+                                graph$nodes_df$descr)
+
+  ### if label is NA, use abbreviated description
+  graph$nodes_df$auto_label = ifelse(is.na(graph$nodes_df$label),
+                                abbreviate(make.names(
+                                  graph$nodes_df$auto_descr,
+                                  TRUE)),
+                                graph$nodes_df$label)
+
+  return(graph)
+}
