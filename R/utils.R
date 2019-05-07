2d4b6a709ac3bf5f3e2cdb37773250fc567ba10e
Nhung Nguyen <nhungnguyen@wifi-roaming-128-4-98-163.host.udel.edu>
2019-05-07 13:10:24 -0400

    Add custom Distribution

--- R/utils.R
+++ R/utils.R
@@ -135,10 +135,11 @@ rhsDecomp = function(rhs) {
   ## if function in greta namespace, then assume distr
   notDistrFunctions = c("%*%","eigen","iprobit","ilogit","colMeans","apply","abind","icloglog","icauchit","log1pe","imultilogit")
 
+  customDistr = c("zero_inflated")
+
   if (is.symbol(distExpr)) {
     fnName = rlang::as_string(distExpr)
-    if (fnName %in% getNamespaceExports("greta") &
-        !(fnName %in% notDistrFunctions)) {
+    if (fnName %in% getNamespaceExports("greta") | fnName %in% customDistr & !(fnName %in% notDistrFunctions)) {
       ## it is a greta distribution - add parantheses so not symbol
       distExpr = rlang::parse_expr(paste0(fnName, "()"))
     } else {
