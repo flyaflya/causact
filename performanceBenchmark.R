ab33931eae87e69896e1fae0a275f75244e30035
Fleischhacker <ajf@udel.edu>
2019-06-07 11:55:24 -0400

    Cleanup files.

--- performanceBenchmark.R
+++
@@ -1,7 +0,0 @@
-library(greta)
-coinflip = rbinom(n = 1000,size = 1,prob = 0.6)
-y <- as_data(coinflip)   #DATA
-theta  <- beta(shape1 = 2, shape2 = 2)   #PRIOR
-distribution(y) <- bernoulli(prob = theta)   #LIKELIHOOD
-gretaModel <- model(theta)   #MODEL
-system.time(draws <- mcmc(gretaModel))   #POSTERIOR
