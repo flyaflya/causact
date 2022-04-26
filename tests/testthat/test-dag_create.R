802a97fd8c2d49bd607758f6ae997f176243da43
flyaflya <ajf@udel.edu>
2022-04-26 16:02:09 -0400

    add more test cases

--- tests/testthat/test-dag_create.R
+++ tests/testthat/test-dag_create.R
@@ -44,4 +44,72 @@ test_that("plate is created", {
   expect_equal(graph$plate_node_df$nodeID, 1)
 })
 
+test_that("graph can be rendered", {
+  renderGraph = dag_create() %>%
+    dag_node("# of Hot Day Beachgoers") %>%
+    dag_node(c("Rent by Location",
+               "Beachgoer Location Probability"),
+             obs = TRUE) %>%
+    dag_node(c("Revenue","Expenses","Profit"),
+             det = TRUE) %>%
+    dag_node("Bathhouse Location",
+             dec = TRUE) %>%
+    dag_edge(from = c("# of Hot Day Beachgoers",
+                      "Beachgoer Location Probability",
+                      "Bathhouse Location"),
+             to = "Revenue") %>%
+    dag_edge(from = c("Bathhouse Location",
+                      "Rent by Location"),
+             to = "Expenses") %>%
+    dag_edge(from = c("Revenue","Expenses"),
+             to = "Profit") %>%
+    dag_render()
+  expect_type(renderGraph, "list")
+})
 
+test_that("diagrammer object can be created", {
+  renderGraph = dag_create() %>%
+    dag_node("# of Hot Day Beachgoers") %>%
+    dag_node(c("Rent by Location",
+               "Beachgoer Location Probability"),
+             obs = TRUE) %>%
+    dag_node(c("Revenue","Expenses","Profit"),
+             det = TRUE) %>%
+    dag_node("Bathhouse Location",
+             dec = TRUE) %>%
+    dag_edge(from = c("# of Hot Day Beachgoers",
+                      "Beachgoer Location Probability",
+                      "Bathhouse Location"),
+             to = "Revenue") %>%
+    dag_edge(from = c("Bathhouse Location",
+                      "Rent by Location"),
+             to = "Expenses") %>%
+    dag_edge(from = c("Revenue","Expenses"),
+             to = "Profit") %>%
+    dag_diagrammer()
+  expect_type(renderGraph, "list")
+})
+
+test_that("greta code is created", {
+  graph = dag_create() %>%
+    dag_node("Get Card","x",
+             rhs = bernoulli(theta),
+             data = carModelDF$getCard) %>%
+    dag_node("Signup Probability","theta",
+             rhs = uniform(0,1),
+             child = "x") %>%
+    dag_plate("Car Model", "y",
+              data = carModelDF$carModel,
+              nodeLabels = "theta",
+              addDataNode = TRUE)  %>%
+    dag_plate("Observations", "i",
+              nodeLabels = c("x","y"))
+  gretaCode = graph %>% dag_greta(mcmc = FALSE)
+  expect_type(gretaCode, "character")
+})
+
+test_that("dag plot creates graph", {
+  df = data.frame(x = c(1:5),y = c(6:10))
+  plotGr = df %>% dagp_plot()
+  expect_type(plotGr, "list")
+})
