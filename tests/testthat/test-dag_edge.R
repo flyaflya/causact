00ae2501b0338f2db1ac89147dd5fa5a360eab61
Adam Fleischhacker <ajf@BUEC-0F00TVX.win.udel.edu>
2023-08-09 08:18:42 -0400

    increasing test coverage after analyzing covr;:report().

--- tests/testthat/test-dag_edge.R
+++ tests/testthat/test-dag_edge.R
@@ -10,4 +10,10 @@ test_that("edge is created", {
   expect_equal(listObj2$edges_df$from, 2)
   expect_equal(listObj1$edges_df$to, 1)
   expect_equal(listObj2$edges_df$to, 1)
+  listObj3 = dag_create() %>%
+    dag_node("Child Node","z") %>%
+    dag_node("Parent Node1","x") %>%
+    dag_node("Parent Node2","y") %>%
+    dag_edge(c("x","y"),"z")
+  expect_type(listObj3, "list")
 })
--- tests/testthat/test-dag_gretaDefunct.R
+++ tests/testthat/test-dag_gretaDefunct.R
@@ -1,4 +1,4 @@
-test_that("numpyro code is created", {
+test_that("dag_greta gives warning", {
   graph = dag_create() %>%
     dag_node("Get Card","x",
              rhs = bernoulli(theta),
@@ -12,9 +12,7 @@ test_that("numpyro code is created", {
               addDataNode = TRUE)  %>%
     dag_plate("Observations", "i",
               nodeLabels = c("x","y"))
-  mcmcCode = graph %>% dag_numpyro(mcmc = FALSE)
-  expect_type(mcmcCode, "character")
-  labelOut = meaningfulLabels(graph)
-  expect_type(labelOut, "NULL")
+  mcmcCode = graph %>% dag_greta(mcmc = FALSE)
+  expect_type(mcmcCode, "NULL")
 })
 
